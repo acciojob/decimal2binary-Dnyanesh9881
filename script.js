0604580686ad69a6924aeab50475c01af1ec5b80
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-18 10:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  let decimal=0;
+	while (num>=1) {
+		decimal=decimal*10+num%2;
+		num=Math.floor(num/2);
+	}
+	return decimal;
 }
 
 window.decimalToBinary = decimalToBinary;
